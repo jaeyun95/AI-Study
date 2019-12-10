8e5399e58de12608894eddf40285176238e08bff
jaeyun95 <wodbs9522@gmail.com>
2019-12-10 15:36:48 +0900

    update

--- tensorflow/example09.py
+++ tensorflow/example09.py
@@ -32,7 +32,7 @@ layer2 = tf.nn.max_pool(layer2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], paddin
 
 
 weight3 = tf.Variable(tf.random_normal([7 * 7 * 64, 256], stddev=0.01))
-layer3 = tf.reshape(L2, [-1, 7 * 7 * 64])
+layer3 = tf.reshape(layer2, [-1, 7 * 7 * 64])
 layer3 = tf.matmul(layer3, weight3)
 layer3 = tf.nn.relu(layer3)
 layer3 = tf.nn.dropout(layer3, keep_prob)
@@ -52,33 +52,17 @@ sess.run(init)
 batch_size = 100
 total_batch = int(mnist.train.num_examples / batch_size)
 
-for epoch in range(15):
+for epoch in range(5):
     total_cost = 0
 
     for i in range(total_batch):
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
         batch_xs = batch_xs.reshape(-1, 28, 28, 1)
-        _, cost_val = sess.run([optimizer, cost], feed_dict={X: batch_xs, Y: batch_ys, keep_prob: 0.8})
+        _, cost_val = sess.run([optimizer, cost], feed_dict={X: batch_xs, Y: batch_ys, keep_prob: 0.7})
         total_cost += cost_val
-    print('Epoch:', '%04d' % (epoch + 1),
-          'Avg. cost =', '{:.3f}'.format(total_cost / total_batch))
+    print('Epoch:', '%04d' % (epoch + 1), 'Avg. cost =', '{:.3f}'.format(total_cost / total_batch))
 
 
 is_correct = tf.equal(tf.argmax(model, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
-print('Accuracy:', sess.run(accuracy,
-                        feed_dict={X: mnist.test.images,
-                                   Y: mnist.test.labels, keep_prob: 1}))
-
-#show images							   
-labels = sess.run(model, feed_dict={X: mnist.test.images, Y: mnist.test.labels, keep_prob: 1})
-fig = plt.figure()
-for i in range(10):
-    subplot = fig.add_subplot(2, 5, i + 1)
-    subplot.set_xticks([])
-    subplot.set_yticks([])
-    subplot.set_title('%d' % np.argmax(labels[i]))
-    subplot.imshow(mnist.test.images[i].reshape((28, 28)),
-                   cmap=plt.cm.gray_r)
-
-plt.show()
+print('Accuracy:', sess.run(accuracy, feed_dict={X: mnist.test.images.reshape(-1, 28, 28, 1), Y: mnist.test.labels, keep_prob: 1}))
