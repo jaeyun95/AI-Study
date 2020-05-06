8ad9823709aebb9a10e1e6e2a9e6e342ab3f3986
jaeyun95 <wodbs9522@gmail.com>
2020-05-07 01:13:54 +0900

    update

--- pytorch/example03.py
+++ pytorch/example03.py
@@ -1,66 +1,91 @@
-#(2) example02
+#(3) example03
 '''
-This is example about treating tensor.
+This is example about simple pytorch neural network
 '''
 import torch
+import numpy as np
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
 
-# tensor generation for treating tensor
-x1 = torch.rand(3,3)
-x2 = torch.rand(3,3)
-print(x1)
-print(x2)
+#######################
+## preparing dataset ##
+#######################
+x_train, y_train = make_blobs(n_samples = 80, n_features = 2, centers=[[1,1],[-1,-1],[1,-1],[-1,1]],shuffle = True, cluster_std = 0.3)
+x_test, y_test = make_blobs(n_samples = 20, n_features = 2, centers=[[1,1],[-1,-1],[1,-1],[-1,1]],shuffle = True, cluster_std = 0.3)
 
-## calculation of tensor
-# plus of tensor
-x3 = x1 + x2
-print(x3)
-# minus of tensor
-x4 = x1 - x2
-print(x4)
-# multiplation of tensor
-x5 = torch.mm(x1,x2)
-print(x5)
-# division of tensor
-x6 = torch.div(x1,x2)
-print(x6)
+## visual function
+def vis_data(x, y = None, c = 'r'):
+    if y is None:
+        y = [None] * len(x)
+    for x_, y_ in zip(x,y):
+        if y_ is None:
+            plt.plot(x_[0], x_[1], '*',markerfacecolor = 'none',markeredgecolor = c)
+        elif y_ == 0:
+            plt.plot(x_[0], x_[1], c+'o')
+        elif y_ == 1:
+            plt.plot(x_[0], x_[1], c+'+')
+        elif y_ == 2:
+            plt.plot(x_[0], x_[1], c+'x')
+        elif y_ == 3:
+            plt.plot(x_[0], x_[1], c+'^')
 
-## dimension tranform of tensor
-# using unsqueeze
-x7 = torch.unsqueeze(x1,0)
-print('before : ',x1.shape,', after : ',x7.shape)
-# using squeeze
-x8 = torch.squeeze(x7)
-print('before : ',x7.shape,', after : ',x8.shape)
-# using view
-x9 = x1.view(9)
-print('before : ',x1.shape,', after : ',x9.shape)
-# using transpose
-tensor_example = torch.rand(4,5)
-x10 = tensor_example.transpose(0,1)
-print('before : ',tensor_example.shape,', after : ',x10.shape)
+#check data graph
+'''
+plt.figure()
+vis_data(x_train, y_train, c = 'r')
+plt.show
+'''
 
-## minimum, maximum of tensor
-# minimum of tensor
-x11 = torch.min(x1)
-min_location = torch.argmin(x1)
-print('minimum : ',x11,', location : ',min_location)
-# maximum of tensor
-x12 = torch.max(x1)
-max_location = torch.argmax(x1)
-print('maximum : ',x12,', location : ',max_location)
+## convert numpy format to tensor format
+x_train = torch.FloatTensor(x_train)
+x_test = torch.FloatTensor(x_test)
+y_train = torch.FloatTensor(y_train)
+y_test = torch.FloatTensor(y_test)
 
-## cat, stack of tensor
-# using cat
-x13 = torch.cat([x1,x2],dim = 0)
-print(x13.shape)
-# using stack
-x14 = torch.stack([x1,x2],dim = 0)
-print(x14.shape)
+#######################
+## defining NN class ##
+#######################
+class NeuralNetwork(torch.nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(NeuralNetwork, self).__init__()
 
-## sum, mean of tensor
-# using sum
-x15 = torch.sum(x1)
-print(x15)
-# using mean
-x16 = torch.mean(x1)
-print(x16)
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.linear_1 = torch.nn.Linear(self.input_size, self.hidden_size)
+        self.relu = torch.nn.ReLU()
+        self.linear_2 = torch.nn.Linear(self.hidden_size, 1)
+        self.sigmoid = torch.nn.Sigmoid()
+
+    def forward(self, input_tensor):
+        linear1 = self.linear_1(input_tensor)
+        relu = self.relu(linear1)
+        linear2 = self.linear_2(relu)
+        output = self.sigmoid(linear2)
+        return output
+
+#######################
+## training for data ##
+#######################
+model = NeuralNetwork(2, 5)
+learning_rate = 0.03
+criterion = torch.nn.BCELoss()
+epochs = 10000
+optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
+
+model.eval()
+test_loss_before = criterion(model(x_test).squeeze(), y_test)
+print('Before Training, test loss is {}'.format(test_loss_before.item()))
+
+for epoch in range(epochs):
+    model.train()
+    optimizer.zero_grad()
+    train_output = model(x_train)
+    train_loss = criterion(train_output.squeeze(), y_train)
+    if epoch%100 == 0:
+        print('Train loss at {} is {}'.format(epoch, train_loss.item()))
+    train_loss.backward()
+    optimizer.step()
+model.eval()
+test_loss_before = criterion(torch.squeeze(model(x_test)),y_test)
+print('After Training, test loss is {}'.format(test_loss_before.item()))
